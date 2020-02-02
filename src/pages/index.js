a7f2fae31d72f5bc045c9d51aa740463c82e14aa
Sunny Singh <me@sunnysingh.io>
2020-02-02 13:02:59 -0500

    Fix outline

--- src/pages/index.js
+++ src/pages/index.js
@@ -83,7 +83,7 @@ export default function HomePage() {
           mb={16}
           as={GatsbyLink}
           to="/categories"
-          variant="solid"
+          variant="outline"
           size="lg"
         >
           Find a resrc
