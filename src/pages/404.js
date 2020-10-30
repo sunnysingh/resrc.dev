c7c0870fb5304ac2a4bd61654ce77b89d1590ccb
Sunny Singh <me@sunnysingh.io>
2020-10-29 22:30:48 -0400

    Add location prop

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,9 +3,9 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Metadata from '../components/Metadata';
 
-export default function NotFoundPage() {
+export default function NotFoundPage({ location }) {
   return (
-    <Layout>
+    <Layout location={location}>
       <Metadata title="404: Not found" />
       <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
